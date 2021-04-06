73e02e0e198eb5c174fbf5056760c34fecc3633c
aida <aida@gmail.com>
2021-04-07 02:11:10 +0600

    last

--- js/restore-access-script.js
+++ js/restore-access-script.js
@@ -1,67 +1,85 @@
 "use strict";
 
-const tokens = {
+const emails = {
+    sultan: {
+        mail: 'sultanscreed@gmail.com',
+        securityToken: '5a8d474f-a4f1-4bf3-af4b-6ff75454e277'
+    },
     developer: {
-        mail: 'aida.developer@mail.ru',
-        securityToken: 'd3450a92-a614-47bc-baa1-8c2d94efb9d2'
+        mail: 'aida.school.developer@protonmail.com',
+        securityToken: '994dbe83-a2a6-45f6-9576-ed72e8827ab0'
     },
     administrator: {
-        mail: 'aida.schoolapp.guardian@mail.ru',
-        securityToken: '6223ac20-e0ef-4106-a133-8cb68fce4ed3',
+        mail: '39.highschool.guardian@gmail.com',
+        securityToken: '9d01f9ca-c3c5-4eba-a74a-d1365d406f15',
     },
     registrator: {
-        mail: 'aida.schoolapp.registrator@mail.ru',
-        securityToken: '039592cb-74d6-4ea6-b81a-d636cb7c81fb'
+        mail: '39.highschool.registrar@gmail.com',
+        securityToken: 'e45e0868-60ea-4082-8ff4-379e6eb43f7a'
     },
     personnelDepartment: {
-        mail: 'aida.schoolapp.personnel.depart@mail.ru',
-        securityToken: '88dd8d6b-cd3f-4c8d-903f-3d462df3d862'
+        mail: '39.highschool.personal.department@gmail.com',
+        securityToken: '57c40788-947a-474d-ba1b-31efb9d567fc'
     }
 };
 
 const contents = {
     toSendto: {
         administrator: {
-            subject: '',
-            body: ''
+            toRestore: {
+                subject: 'administrator!',
+                body: '<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>'
+            }
         },
         registrator: {
-            subject: '',
-            body: ''
+            toRestore: {
+                subject: 'registrator!',
+                body: '<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>'
+            }
         },
         personnelDepartment: {
-            subject: '',
-            body: ''
+            toRestore: {
+                subject: 'personnelDepartment!',
+                body: '<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>'
+            }
         }
     }
 };
 
 const accordionButton = document.querySelector('.accordion-button');
 
-function sendingEmail(selectedToken) {
+function sendEmail(selectedToken, sendTo, sendFrom, content) { //selectedToken, to, from, content
     Email.send({
-        SecureToken : "<your generated token>",
-        To : 'recipient@example.com',
-        From : "sender@example.com",
-        Subject : "Test Email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-        }).then(
-            message => alert("mail sent successfully")
-        );
+        SecureToken: emails.sultan.securityToken,
+        To: emails.sultan.mail,
+        From: emails.sultan.mail,
+        Subject: contents.toSendto.administrator.subject,
+        Body: contents.toSendto.administrator.body //"<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+        message => alert("mail sent successfully")
+    );
 }
 
 function emailSendingButton() {
 
     switch (accordionButton.textContent) {
-        case 'Бөлімді таңдаңыз': 
+        case 'Бөлімді таңдаңыз':
             break;
         case 'Администратор':
+            sendEmail();
+            // sendEmail(mails.developer.securityToken,
+            // mails.administrator.mail, mails.developer.mail, 
+            // contents.toSendto.administrator);
+            console.log(mails.developer.securityToken,
+                mails.administrator.mail, mails.developer.mail,
+                contents.toSendto.administrator.body);
             break;
         case 'Регистратор':
             break;
         case 'Кадрлар бөлімі':
             break;
-        default: console.log('asdasd');
+        default:
+            console.log('asdasd');
     }
 
 }
@@ -69,17 +87,17 @@ function emailSendingButton() {
 
 function isClicked(selectedA) {
     console.log('123456');
-    const selectedAContent = document.getElementById(selectedA); 
+    const selectedAContent = document.getElementById(selectedA);
     switch (selectedA) {
         case 'firstA':
             accordionButton.innerHTML = selectedAContent.textContent;
-        break;
+            break;
         case 'secondA':
             accordionButton.innerHTML = selectedAContent.textContent;
-        break;
+            break;
         case 'thirdA':
             accordionButton.innerHTML = selectedAContent.textContent;
-        break;
+            break;
     }
     accordionButton.click();
 }
